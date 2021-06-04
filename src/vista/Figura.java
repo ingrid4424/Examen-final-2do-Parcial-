16c7a20eaeef3c233a09d94e37476d038e5ed621
Ingrid Vallecilla <stepha4424@gmail.com>
2021-06-04 18:29:57 -0500

    creacion de figura

--- src/vista/Figura.java
+++ src/vista/Figura.java
@@ -1,24 +1,44 @@
 package vista;
 
+import processing.core.PApplet;
+
 public abstract class Figura extends Thread{
 	
 	protected float posX;
 	protected float posY;
+	protected int sizeX;
+	protected int sizeY;
+	protected PApplet app;
 	
-	public Figura() {
-		
+	public Figura(PApplet app, float posX, float posY, int sizeX, int sizeY) {
+		this.app = app;
+		this.posX = posX;
+		this.posY = posY;
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
 	}
 
 	public void run() {
 		try {
-			
+			System.out.println("Se ha ejecutado el programa");
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
 	
-	public void pintar() {
-		
+	public void pintar(int r, int g,int b, int tipo) {
+		switch (tipo) {
+		case 0:
+			this.app.noStroke();
+			this.app.fill(r, g, b);
+			this.app.ellipse(posX, posY, sizeX, sizeY);
+			break;
+		case 1:
+			this.app.stroke(r, g, b);
+			this.app.fill(255);
+			this.app.ellipse(posX, posY, sizeX, sizeY);
+			break;
+		}
 	}
 	
 	public abstract void mover();
