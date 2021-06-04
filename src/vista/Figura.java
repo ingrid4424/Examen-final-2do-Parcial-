3374bfe8af3a7078b163811896f7356523feb34d
Ingrid Vallecilla <stepha4424@gmail.com>
2021-06-04 17:59:16 -0500

    first commit

---
+++ src/vista/Figura.java
@@ -0,0 +1,25 @@
+package vista;
+
+public abstract class Figura extends Thread{
+	
+	protected float posX;
+	protected float posY;
+	
+	public Figura() {
+		
+	}
+
+	public void run() {
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	public void pintar() {
+		
+	}
+	
+	public abstract void mover();
+}
---
+++ src/vista/Principal.java
@@ -0,0 +1,12 @@
+package vista;
+
+import processing.core.PApplet;
+
+public class Principal extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(Principal.class.getName());
+	}
+
+}
