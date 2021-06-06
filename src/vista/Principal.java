e673d9ae6bfeda137d2ca562d0039bcb5e6bf0e3
Ingrid Vallecilla <stepha4424@gmail.com>
2021-06-05 21:44:16 -0500

    LaLLevaStyle

--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -14,24 +14,32 @@ public class Principal extends PApplet{
 	}
 	
 	ArrayList<Polo> polos;
-	Marco marco;
-	
+	//Marco marco;
+	// prueba para hacer marcos
+	ArrayList<Marco> marcos;
 	public void settings() {
 		size(800,600);
 	}
 	
 	public void setup() {
 		polos = new ArrayList<Polo>();
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 30; i++) {
 			polos.add(new Polo(this, random(700), random(500), 30, 30, 2, 2));
 			
 		}
-		marco = new Marco(this, 50, 50, 30, 30, 1, 1);
-		marco.start();
 		
 		for (Polo polo : polos) {
 			polo.start();
 		}
+		/*marco = new Marco(this, 50, 50, 30, 30, 1, 1);
+		marco.start();*/
+		
+		//elementos para lo de marcos
+		marcos = new ArrayList<Marco>();
+		marcos.add(new Marco(this, 50, 50, 30, 30, 1, 1));
+		marcos.get(0).start();
+		
+		
 		
 		
 	
@@ -46,19 +54,43 @@ public class Principal extends PApplet{
 				polo.pintar(255, 150, 150, 1);
 				polo.pintarMensaje();
 				polo.mover();
-				polo.setOnLlamando(marco.isOnLLamar());
 				
 			}
 			
-			marco.pintar(0, 0, 255, 0);
+			
+			
+			
+			for (Polo polo : polos) {
+				for (Marco marco : marcos) {
+					polo.setOnLlamando(marco.isOnLLamar());
+				}
+				
+				
+			}
+			
+			/*marco.pintar(0, 0, 255, 0);
 			marco.pintarMensaje();
-			marco.mover();
+			marco.mover();*/
+			
+			//////////////////
+			
+			for (Marco marco : marcos) {
+				marco.pintar(0, 0, 255, 0);
+				marco.pintarMensaje();
+				marco.mover();
+				
+				if(marco.isOnLLamar()) {
+					calcularTarget();
+				}
+			}
+			
+			//////////////////
 			
 			eliminarElementos();
 			
-			if(marco.isOnLLamar()) {
+			/*if(marco.isOnLLamar()) {
 				calcularTarget();
-			}
+			}*/
 			
 			
 		} catch (Exception e) {
@@ -77,13 +109,19 @@ public class Principal extends PApplet{
 						try {
 							Thread.sleep(2000);
 							for (int i = 0; i< polos.size(); i++) {
-								float ditanciaMarco = dist(polos.get(i).getPosX(), polos.get(i).getPosY(), marco.getPosX(), marco.getPosY());
-								polos.get(i).setDistanciaMarco(ditanciaMarco);
+								for (Marco marco : marcos) {
+									float ditanciaMarco = dist(polos.get(i).getPosX(), polos.get(i).getPosY(), marco.getPosX(), marco.getPosY());
+									polos.get(i).setDistanciaMarco(ditanciaMarco);
+								}
+								
+								
 							}
 							
 							Collections.sort(polos);
+							for (Marco marco : marcos) {
+								marco.setTarget(polos.get(0).getPosX(), polos.get(0).getPosY());
+							}
 							
-							marco.setTarget(polos.get(0).getPosX(), polos.get(0).getPosY());
 							
 						} catch (Exception e) {
 							// TODO: handle exception
@@ -101,11 +139,17 @@ public class Principal extends PApplet{
 	
 	public void eliminarElementos() {
 		for (int i = 0; i< polos.size(); i++) {
-			float ditanciaMarco = dist(polos.get(i).getPosX(), polos.get(i).getPosY(), marco.getPosX(), marco.getPosY());
-			if(ditanciaMarco<30) {
-				polos.remove(polos.get(i));
-				System.out.println("eliminado");
+			for (Marco marco : marcos) {
+				float ditanciaMarco = dist(polos.get(i).getPosX(), polos.get(i).getPosY(), marco.getPosX(), marco.getPosY());
+				if(ditanciaMarco<30) {
+					marcos.add(new Marco(this, polos.get(i).getPosX(), polos.get(i).getPosY(), 30, 30, 2, 2));
+					polos.remove(polos.get(i));
+					
+					
+					System.out.println("eliminado");
+				}
 			}
+			
 		}
 	}
 
